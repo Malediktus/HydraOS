ee5d04330d2e40328ff9dd00c2ee6daa5161297d
Nico Grundei <malediktusrgb@gmail.com>
2024-05-23 23:20:25 +0200

    feat(kernel): integrate printf implementation (from https://github.com/mpaland/printf)

--- kernel/src/misc/kprintf.c
+++ kernel/src/misc/kprintf.c
@@ -10,6 +10,21 @@ void _putchar(char c)
 
 // from https://github.com/mpaland/printf
 
+// Copyright 2014 Marco Paland
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef PRINTF_NTOA_BUFFER_SIZE
 #define PRINTF_NTOA_BUFFER_SIZE 32U
 #endif
