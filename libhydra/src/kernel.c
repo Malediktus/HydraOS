c5919f1b0e8b8fa0387e27037f2fd29516d00431
Malediktus <malediktusrgb@gmail.com>
2024-08-02 16:50:38 +0200

    feat(libhydra): implement new syscall wrappers for user mode

--- libhydra/src/kernel.c
+++ libhydra/src/kernel.c
@@ -18,3 +18,28 @@ uint64_t syscall(uint64_t num, uint64_t arg1, uint64_t arg2, uint64_t arg3, uint
 
     return result;
 }
+
+uint64_t syscall_read(uint64_t stream, uint8_t *data, size_t size)
+{
+    return syscall(_SYSCALL_READ, stream, (uint64_t)data, size, 0, 0, 0);
+}
+
+uint64_t syscall_write(uint64_t stream, const uint8_t *data, size_t size)
+{
+    return syscall(_SYSCALL_WRITE, stream, (uint64_t)data, size, 0, 0, 0);
+}
+
+uint64_t syscall_fork(void)
+{
+    return syscall(_SYSCALL_FORK, 0, 0, 0, 0, 0, 0);
+}
+
+void syscall_exit(uint32_t result)
+{
+    return syscall(_SYSCALL_EXIT, result, 0, 0, 0, 0, 0);
+}
+
+uint64_t syscall_ping(uint64_t pid)
+{
+    return syscall(_SYSCALL_PING, pid, 0, 0, 0, 0, 0);
+}
